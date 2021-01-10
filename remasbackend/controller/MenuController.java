17184b3e58bcedf997ba7854ff35e2ed69501094
batuhanakc <73386410+batuhanakc@users.noreply.github.com>
2021-01-10 19:15:09 +0300

    Update MenuController.java

--- remasbackend/controller/MenuController.java
+++ remasbackend/controller/MenuController.java
@@ -32,7 +32,7 @@ public class MenuController {
     return menuRepository.findAll();
     }
     
-    @PostMapping("/reservations")
+    @PostMapping("/menu")
     public Menu createMenu(@RequestBody Menu admin){
     return menuRepository.save(admin);
     }
